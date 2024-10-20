3300b41b1f6919c69119e4f447fef918662144a1
Mohammad Afeef <mohammadafeef784@gmail.com>
2024-10-20 16:13:57 +0530

    Tested

--- Mark_Attendence_by_stevin/src/main.cpp
+++ Mark_Attendence_by_stevin/src/main.cpp
@@ -8,21 +8,16 @@
 
 #include <MFRC522.h>
 
+#include <TimeLib.h>
+
 #include <WiFiUdp.h>
 
 #include <NTPClient.h>
 
-  
-
-
-
-#define WIFI_SSID ""
-
-#define WIFI_PASSWORD ""
-
-#define DATABASE_URL ""
-
-  
+// hidden code id : 1
+#define WIFI_SSID "ARC-WIFI"
+#define WIFI_PASSWORD "Arc@#Pass156"
+#define DATABASE_URL "arc-lock-6e5c6-default-rtdb.asia-southeast1.firebasedatabase.app"
 
 FirebaseData fbdo;
 
@@ -30,17 +25,11 @@ FirebaseAuth auth;
 
 FirebaseConfig config;
 
-  
-
 const int relayPin = D2;
 
 const int buzzerPin = D1;
 
-  
-
 const int wifiWaitTime = 15000;
-
-  
 
 #define SS_PIN D4
 
@@ -48,293 +37,309 @@ const int wifiWaitTime = 15000;
 
 MFRC522 rfid(SS_PIN, RST_PIN);
 
-  
-
 String cardID = "";
 
 WiFiUDP ntpUDP;
 
-NTPClient timeClient(ntpUDP, "pool.ntp.org", 19800, 60000);
-
-
+NTPClient timeClient(ntpUDP, "in.pool.ntp.org");
 
 void beep(int type)
 
 {
 
-if (type == 1)
+	if (type == 1)
 
+	{
+
+		digitalWrite(buzzerPin, HIGH);
+
+		delay(250);
+
+		digitalWrite(buzzerPin, LOW);
+
+		delay(250);
+
+		digitalWrite(buzzerPin, HIGH);
+
+		delay(250);
+
+		digitalWrite(buzzerPin, LOW);
+
+		delay(250);
+	}
+
+	else if (type == 2)
+
+	{
+
+		digitalWrite(buzzerPin, HIGH);
+
+		delay(500);
+
+		digitalWrite(buzzerPin, LOW);
+
+		delay(500);
+	}
+}
+
+void markAttendance(String cardID)
 {
-
-digitalWrite(buzzerPin, HIGH);
-
-delay(250);
-
-digitalWrite(buzzerPin, LOW);
-
-delay(250);
-
-digitalWrite(buzzerPin, HIGH);
-
-delay(250);
-
-digitalWrite(buzzerPin, LOW);
-
-delay(250);
-
-}
-
-else if (type == 2)
-
-{
-
-digitalWrite(buzzerPin, HIGH);
-
-delay(500);
-
-digitalWrite(buzzerPin, LOW);
-
-delay(500);
-
-}
-
-}
-
-void markAttendance(String cardID) {
 	timeClient.update();
-  unsigned long epochTime = timeClient.getEpochTime();
-  struct tm *ptm = gmtime ((time_t *)&epochTime);
-	String currentDate = String(ptm->tm_mday)+"-"+String(ptm->tm_mon+1)+"-"+String(ptm->tm_year+1900);
-	String month_year = String(ptm->tm_mon+1)+"-"+String(ptm->tm_year+1900);
-	String access_time =  Firebase.getString(fbdo, "access/" + cardID + "/last_access")? fbdo.to<const char *>() : fbdo.errorReason().c_str();
+	unsigned long epochTime = timeClient.getEpochTime();
+	String currentDate = String(day(epochTime)) + "-" + String(month(epochTime)) + "-" + String(year(epochTime));
+	String month_year = String(month(epochTime)) + "-" + String(year(epochTime));
+	String access_time = Firebase.getString(fbdo, "access/" + cardID + "/last_access") ? fbdo.to<const char *>() : fbdo.errorReason().c_str();
 
-	if(currentDate != access_time){
-		String name = Firebase.getString(fbdo, "access/" + cardID + "/name") ? fbdo.to<const char *>():"blank";
+	if (currentDate != access_time)
+	{
+		if (Firebase.getString(fbdo, "access/" + cardID + "/Name"))
+		{
+			String name = fbdo.to<const char *>();
+			String path = "/attendance/" + month_year + "/" + name;
 
-		String path = "/attendance/"+month_year+"/" + name +"/count";
+			if (Firebase.getInt(fbdo, path))
+			{
 
-		if (Firebase.getInt(fbdo, path)) {
+				int currentCount = fbdo.to<int>();
 
-			int currentCount = fbdo.to<int>();
+				currentCount++;
 
-			currentCount++;
+				if (Firebase.setInt(fbdo, path, currentCount))
+				{
 
-			if (Firebase.setInt(fbdo, path, currentCount)) {
+					Serial.println("Attendance marked for: " + cardID + ". New Count: " + String(currentCount));
+					Firebase.setString(fbdo, "access/" + cardID + "/last_access/", currentDate);
+					Firebase.setString(fbdo, "access/" + cardID + "/entry_time/", timeClient.getFormattedTime());
+					Firebase.setString(fbdo, "access/" + cardID + "/exit_time/", "null");
+				}
+				else
+				{
 
-				Serial.println("Attendance marked for: " + cardID + ". New Count: " + String(currentCount));
-				Firebase.setString(fbdo, "access/" + cardID + "/last_access/",currentDate);
+					Serial.println("Error updating attendance:");
+				}
 			}
-			else {
+			else
+			{
 
-				Serial.println("Error updating attendance:");
+				if (Firebase.setInt(fbdo, path, 1))
+				{
 
+					Serial.println("Attendance marked for: " + cardID + ". New Count: 1");
+					Firebase.setString(fbdo, "access/" + cardID + "/last_access/", currentDate);
+					Firebase.setString(fbdo, "access/" + cardID + "/entry_time/", timeClient.getFormattedTime());
+					Firebase.setString(fbdo, "access/" + cardID + "/exit_time/", "null");
+				}
+				else
+				{
+
+					Serial.println("Error updating attendance:");
+				}
 			}
 		}
+		else
+		{
+			Serial.println("Name not found");
+		}
 	}
-	else{
+	else
+	{
 		Serial.println("Attendance is already marked");
+		Firebase.setString(fbdo, "access/" + cardID + "/exit_time/", timeClient.getFormattedTime());
 	}
-
 }
 void setup()
 
 {
 
-Serial.begin(9600);
+	Serial.begin(9600);
 
-WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-Serial.print("Connecting to Wi-Fi");
+	Serial.print("Connecting to Wi-Fi");
 
-int i = 0;
+	int i = 0;
 
-while (WiFi.status() != WL_CONNECTED && i < wifiWaitTime)
+	while (WiFi.status() != WL_CONNECTED && i < wifiWaitTime)
 
-{
+	{
 
-Serial.print(".");
+		Serial.print(".");
 
-delay(300);
+		delay(300);
 
-i += 300;
+		i += 300;
+	}
 
+	if (WiFi.status() == WL_CONNECTED)
+
+	{
+
+		Serial.println();
+
+		Serial.print("Connected with IP: ");
+
+		Serial.println(WiFi.localIP());
+
+		Serial.println();
+	}
+
+	if (i >= wifiWaitTime)
+
+	{
+
+		Serial.println();
+
+		Serial.println("Connection failed");
+
+		Serial.println();
+	}
+
+	config.signer.test_mode = true;
+
+	config.database_url = DATABASE_URL;
+
+	// hiden code id : 2
+	config.signer.tokens.legacy_token = "EJf6A6wWjYycd06bLoLzD4ziGJjiwcdpHwzOxFYq";
+
+	Firebase.begin(&config, &auth);
+
+	Firebase.reconnectWiFi(true);
+
+	Firebase.setDoubleDigits(5);
+
+	pinMode(relayPin, OUTPUT);
+
+	digitalWrite(relayPin, LOW);
+
+	pinMode(buzzerPin, OUTPUT);
+
+	digitalWrite(buzzerPin, LOW);
+
+	SPI.begin(); // Init SPI bus
+
+	rfid.PCD_Init(); // Init MFRC522 module
+	timeClient.begin();
+	timeClient.setTimeOffset(19800); // UTC+5:30 (Central European Time)
+
+	Serial.println("Scan an RFID tag...");
 }
-
-if (WiFi.status() == WL_CONNECTED)
-
-{
-
-Serial.println();
-
-Serial.print("Connected with IP: ");
-
-Serial.println(WiFi.localIP());
-
-Serial.println();
-
-}
-
-if (i >= wifiWaitTime)
-
-{
-
-Serial.println();
-
-Serial.println("Connection failed");
-
-Serial.println();
-
-}
-
-  
-
-config.signer.test_mode = true;
-
-config.database_url = DATABASE_URL;
-
-//hiden code id : 2
-
-config.signer.tokens.legacy_token = "";
-
-  
-
-Firebase.begin(&config, &auth);
-
-Firebase.reconnectWiFi(true);
-
-Firebase.setDoubleDigits(5);
-
-  
-
-pinMode(relayPin, OUTPUT);
-
-digitalWrite(relayPin, LOW);
-
-pinMode(buzzerPin, OUTPUT);
-
-digitalWrite(buzzerPin, LOW);
-
-  
-
-SPI.begin(); // Init SPI bus
-
-rfid.PCD_Init(); // Init MFRC522 module
-
-Serial.println("Scan an RFID tag...");
-
-}
-
-  
 
 void loop()
 
 {
 
-cardID = "";
+	cardID = "";
+	if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial())
 
-if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial())
+	{
 
-{
+		// Convert UID to a string
 
-// Convert UID to a string
+		Serial.print("Card UID: ");
 
-Serial.print("Card UID: ");
+		for (byte i = 0; i < rfid.uid.size; i++)
 
-for (byte i = 0; i < rfid.uid.size; i++)
+		{
 
-{
+			// Add each byte to the cardID string in HEX format
 
-// Add each byte to the cardID string in HEX format
+			cardID += String(rfid.uid.uidByte[i] < 0x10 ? "0" : "");
 
-cardID += String(rfid.uid.uidByte[i] < 0x10 ? "0" : "");
+			cardID += String(rfid.uid.uidByte[i], HEX);
+		}
 
-cardID += String(rfid.uid.uidByte[i], HEX);
+		Serial.println(cardID); // Print the card ID stored in the string
 
-}
+		if (cardID == "43a20d2e" || cardID == "a1c79100" || cardID == "c9d19100" || cardID == "73f09100" || cardID == "734d6030" || cardID == "f31df106")
 
-Serial.println(cardID); // Print the card ID stored in the string
+		{
 
-if (cardID == "43a20d2e" || cardID == "a1c79100" || cardID == "c9d19100" || cardID == "73f09100" || cardID == "734d6030" || cardID == "f31df106")
+			digitalWrite(relayPin, HIGH);
 
-{
+			beep(1);
 
-digitalWrite(relayPin, HIGH);
+			delay(3000);
 
-beep(1);
+			digitalWrite(relayPin, LOW);
 
-delay(3000);
+			markAttendance(cardID);
+		}
 
-digitalWrite(relayPin, LOW);
+		else
 
-markAttendance(cardID);
+		{
 
-}
+			String access = Firebase.getString(fbdo, "access/" + cardID + "/status") ? fbdo.to<const char *>() : fbdo.errorReason().c_str();
 
-else
+			Serial.println(access);
 
-{
+			if (access == "granted")
 
-String access = Firebase.getString(fbdo, "access/" + cardID + "/status") ? fbdo.to<const char *>() : fbdo.errorReason().c_str();
+			{
 
-Serial.println(access);
+				digitalWrite(relayPin, HIGH);
+				beep(1);
+				delay(3000);
+				digitalWrite(relayPin, LOW);
+				markAttendance(cardID);
+			}
 
-  
+			else
 
-if (access == "granted")
+			{
 
-{
+				if (access == "path not exist")
 
-digitalWrite(relayPin, HIGH);
-beep(1);
-delay(3000);
-digitalWrite(relayPin, LOW);
-markAttendance(cardID);
+				{
 
-}
+					Firebase.setString(fbdo, "access/" + cardID + "/status", "denied") ? "ok" : fbdo.errorReason().c_str();
+					Firebase.setString(fbdo, "access/" + cardID + "/Name", "Guest") ? "ok" : fbdo.errorReason().c_str();
+					Firebase.setString(fbdo, "access/" + cardID + "/last_access/", "");
+				}
 
-else
+				beep(2);
+			}
+		}
 
-{
+		// Halt PICC and stop encryption on PCD
 
-if (access == "path not exist")
+		rfid.PICC_HaltA();
 
-{
+		rfid.PCD_StopCrypto1();
+	}
 
-Firebase.setString(fbdo, "access/" + cardID + "/status", "denied") ? "ok" : fbdo.errorReason().c_str();
-Firebase.setString(fbdo, "access/" + cardID + "/Name", "Guest") ? "ok" : fbdo.errorReason().c_str();
+	String lock = Firebase.getString(fbdo, "lock") ? fbdo.to<const char *>() : fbdo.errorReason().c_str();
 
-}
+	if (lock == "open")
 
-beep(2);
+	{
 
-}
+		digitalWrite(relayPin, HIGH);
 
-}
+		beep(1);
 
-// Halt PICC and stop encryption on PCD
+		delay(3000);
 
-rfid.PICC_HaltA();
+		digitalWrite(relayPin, LOW);
 
-rfid.PCD_StopCrypto1();
+		Firebase.setString(fbdo, "lock", "close") ? "ok" : fbdo.errorReason().c_str();
+	}
+	if (Serial.available()) {
+    String receivedData = Serial.readStringUntil('\n');  // Read the entire string
+    
+    Serial.print("Received: ");
+    Serial.println(receivedData);
 
-}
-
-String lock = Firebase.getString(fbdo, "lock") ? fbdo.to<const char *>() : fbdo.errorReason().c_str();
-
-if (lock == "open")
-
-{
-
-digitalWrite(relayPin, HIGH);
-
-beep(1);
-
-delay(3000);
-
-digitalWrite(relayPin, LOW);
-
-Firebase.setString(fbdo, "lock", "close") ? "ok" : fbdo.errorReason().c_str();
-
-}
-
+    // to check karna sirf ek hi string ho aur access
+    if (receivedData == "arc@123") {
+    	digitalWrite(relayPin, HIGH);
+		beep(1);
+		delay(3000);
+		digitalWrite(relayPin, LOW);
+    } else {
+		beep(2);
+      Serial.print("Invalid input: ");
+      Serial.println(receivedData);
+    }
+  }
 }
